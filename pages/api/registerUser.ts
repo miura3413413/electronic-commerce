84b764f664ad14a18cec781aaa19266ae2dae129
miura3413413 <114330852+miura3413413@users.noreply.github.com>
2022-11-26 23:41:27 +0900

    test post api

--- pages/api/registerUser.ts
+++ pages/api/registerUser.ts
@@ -13,6 +13,7 @@ export default async function handler(
 ) {
   await db.connectMongo()
   try {
+    res.setHeader('Access-Control-Allow-Origin', "process.env.NEXT_PUBLIC_BASE_URL")
     const newUser = await new User(req.body)
     const user = await newUser.save()
     return res.status(200).json(user);
