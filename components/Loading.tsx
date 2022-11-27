415a1d1f6ba03d17e846a9e3d56d6de741170a5b
miura3413413 <114330852+miura3413413@users.noreply.github.com>
2022-11-27 16:48:09 +0900

    t

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -4,7 +4,8 @@ import React from 'react'
 
 const Loading:NextPage = () => {
   return (
-    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+    <div className='h-screen w-screen flex  justify-center items-center'>
+      <div className="mr-2 animate-spin h-8 w-8 border-4 border-blue-500 rounded-full border-t-transparent"></div>
       <h1 className='text-2xl font-bold'>Loading....</h1>
     </div>
   )
